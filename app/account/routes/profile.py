d989892a1e520e5ec72782f493cd0467f4959fee
Chandra Deb <chandra.deb.sc@outlook.com>
2024-07-26 03:55:20 -0700

    create profile route

--- app/account/routes/profile.py
+++ app/account/routes/profile.py
@@ -1,8 +1,9 @@
-from flask import  render_template
-from flask_login import current_user
+from flask import render_template
+from flask_login import current_user, login_required
 from app.account import account
 
 
 @account.route('/profile')
+@login_required
 def profile():
     return render_template('profile.html', title='Profile', user=current_user)
