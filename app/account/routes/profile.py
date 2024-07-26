8a41e91ef9c6915584e2efca58dc434538ea69f2
Chandra Deb <chandra.deb.sc@outlook.com>
2024-07-26 09:49:40 -0700

    manageable reason to import like this

--- app/account/routes/profile.py
+++ app/account/routes/profile.py
@@ -1,5 +1,9 @@
 from flask import render_template
 from flask_login import current_user, login_required
+# I did not choose:
+# from app import account
+# just to distinct between things directly coming from app package
+# or from another package inside app package
 from app.account import account
 
 
