72d3fbd09c18b82520606fcf9e5b18e46415a230
Chandra Deb <chandra.deb.sc@outlook.com>
2024-07-25 22:33:35 -0700

    complete login route

--- app/auth/routes/signin.py
+++ app/auth/routes/signin.py
@@ -1,9 +1,26 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user
+import sqlalchemy as sa
 from app.auth import auth
 from app.auth.forms.signin_form import SignInForm
+# from app.account import account
+from app import db
+from app.models import User
+
+# ...
 
 
-@auth.route('/signin')
+@auth.route('/signin', methods=['GET', 'POST'])
 def signin():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = SignInForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('signin'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('account.profile'))
     return render_template('signin.html', title='Sign In', form=form)
