6f76737401a4b9f999713bbfcf20fb48b9fa1a17
Chandra Deb <chandra.deb.sc@outlook.com>
2024-07-26 15:44:21 +0800

    update dashboard repository to have few more properties

--- app/account/repositories/dashboard_repository.py
+++ app/account/repositories/dashboard_repository.py
@@ -14,6 +14,33 @@ class CharacterStatus(Enum):
 
 
 class DashboardRepository:
-    def get_mastered_chars_count(self) -> int:
-        return db.session.execute(db.select(db.func.count()).select_from(UserCharacterStatus)
-                                  .filter_by(user_id=current_user.id, status='mastered')).scalar()
+
+    def __get_char_len_with_given_status(self, status: CharacterStatus) -> int:
+        return db.session.execute(
+            db.select(db.func.count()).select_from(UserCharacterStatus)
+            .filter_by(user_id=current_user.id, status=status.value)).scalar()
+
+    @property
+    def learning_chars_len(self) -> int:
+        return self.__get_char_len_with_given_status(CharacterStatus.learning)
+    @property
+    def mastered_chars_len(self) -> int:
+        return self.__get_char_len_with_given_status(CharacterStatus.mastered)
+
+    @property
+    def assumed_chars_len(self) -> int:
+        return self.__get_char_len_with_given_status(CharacterStatus.assumed)
+
+    @property
+    def blocked_chars_len(self) -> int:
+        return self.__get_char_len_with_given_status(CharacterStatus.blocked)
+
+    @property
+    def hidden_chars_len(self) -> int:
+        return self.__get_char_len_with_given_status(CharacterStatus.hidden)
+
+    @property
+    def hard_chars_len(self) -> int:
+        return self.__get_char_len_with_given_status(CharacterStatus.hard)
+
+
