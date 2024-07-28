fecbbfafe7089bec1579f4eb8ee7b5aba048f681
Chandra Deb <chandra.deb.sc@outlook.com>
2024-07-28 17:50:38 +0800

    add function to get all the characters by given list id

--- app/character/services/lists_service.py
+++ app/character/services/lists_service.py
@@ -1,5 +1,7 @@
+from typing import List
+
 from app.character.repositories.lists_repository import ListsRepository
-from app.models import CharacterList
+from app.models import CharacterList, ChineseCharacter
 
 
 class ListsService:
@@ -8,6 +10,9 @@ class ListsService:
 
     def get_list_by_id(self, list_id) -> CharacterList:
         return self.repository.get_list_by_id(list_id)
+
+    def get_characters_by_list_id(self, list_id) -> list[ChineseCharacter]:
+        return self.get_list_by_id(list_id).characters
 
     def get_all_parent_lists(self, list_id) -> list[CharacterList]:
         return self.repository.get_all_parent_lists(list_id)
