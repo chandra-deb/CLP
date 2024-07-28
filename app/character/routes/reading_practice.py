f6ee6e620104078f6b2950904134b122f4dafefa
Chandra Deb <chandra.deb.sc@outlook.com>
2024-07-28 18:25:45 +0800

    setup the stage for reading practice...yay!!!

--- app/character/routes/reading_practice.py
+++ app/character/routes/reading_practice.py
@@ -1,3 +1,5 @@
+from random import choice
+
 from flask import render_template
 
 from app.character import character
@@ -7,5 +9,7 @@ from app.character.services import lists_service
 @character.route('/reading_practice/<int:list_id>')
 def reading_practice(list_id):
     characters = lists_service.get_characters_by_list_id(list_id)
+    random_character = choice(characters)
+    current_list = lists_service.get_list_by_id(list_id)
     return render_template('reading_practice.html',
-                           title='Reading Practice', characters=characters)
+                           title='Reading Practice', character=random_character,  list=current_list)
