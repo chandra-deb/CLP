f5557058551aa160331a15acc0d4e289e6850c1c
Chandra Deb <chandra.deb.sc@outlook.com>
2024-07-28 11:55:09 +0800

    using id approach instead of name approach to get sub lists

--- app/character/routes/sublists.py
+++ app/character/routes/sublists.py
@@ -3,30 +3,44 @@ from flask_login import current_user
 
 from app.character import character
 from app.models import CharacterList
+from app import db
+
+def get_all_parents(list_id):
+    # Create a CTE to recursively get all parents
+    parent_cte = db.session.query(CharacterList).\
+        filter(CharacterList.id == list_id).\
+        cte(recursive=True)
+
+    # Define the recursive query
+    parent_cte = parent_cte.union_all(
+        db.session.query(CharacterList).\
+            filter(CharacterList.id == parent_cte.c.parent_list_id)
+    )
+
+    # Get all parents
+    all_parents = db.session.query(parent_cte).all()
+
+    # Return the parent list objects
+    reversed_parents = all_parents[::-1]
+    return reversed_parents[:-1]
 
 
-@character.route('/lists/<path:list_path>')
-def sublists(list_path):
-    print(list_path)
-    list_path_parts = list_path.split('/')
-    current_list = CharacterList.query.filter_by(name=list_path_parts[0], parent_list=None,
-                                                 # user_id=current_user.id
-                                                 ).first()
+@character.route('/lists/<int:list_id>')
+def sublists(list_id):
+    # Parent lists
+    parents = get_all_parents(list_id)
+    #
+
+    current_list = CharacterList.query.get(list_id)
     if current_list is None:
         return 'List not found', 404
-    list_path_so_far = [list_path_parts[0]]
-    print(list_path_so_far)
-    for part in list_path_parts[1:]:
-        current_list = CharacterList.query.filter_by(name=part, parent_list=current_list,
-                                                     # user_id=current_user.id
-                                                     ).first()
-        if current_list is None:
-            return 'List not found', 404
+    child_lists = current_list.child_lists
+    print(current_list.parent_list)
+    print('Sublists: ')
+    print(child_lists)
 
-        list_path_so_far.append(part)
-    sublists = current_list.child_lists
-    list_path_so_far.pop(len(list_path_so_far) - 1)
-    print(list_path_so_far)
+    print('Parents')
+    print(parents)
     characters = current_list.characters
-    return render_template('sublists.html', list=current_list, sublists=sublists, characters=characters,
-                           list_path=list_path, list_path_so_far=list_path_so_far)
+    return render_template('sublists.html', list=current_list, child_lists=child_lists, characters=characters,
+                           parents=parents)
