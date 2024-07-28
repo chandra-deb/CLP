a5d1c4ae32e98da6a8a3a8395c45e1b0237ece6f
Chandra Deb <chandra.deb.sc@outlook.com>
2024-07-28 16:48:40 +0800

    lists and sublists routes now make use of that lists service

--- app/character/routes/lists.py
+++ app/character/routes/lists.py
@@ -1,24 +1,16 @@
 from flask import render_template
-from flask_login import login_required, current_user
 
-from app import db
 from app.character import character
-from app.character.services import list_service
-from app.models import CharacterList
+from app.character.services import lists_service
 
 
 # Revise 1
 # !! This codes also works well
 @character.route('/lists')
 def lists():
-    top_level_user_lists = db.session.scalars(
-        db.select(CharacterList)
-        .where(CharacterList.user_id == current_user.id)
-        .where(CharacterList.parent_list == None)
-    ).all()
-    top_level_premade_lists = db.session.scalars(
-        db.select(CharacterList)
-        .where(CharacterList.is_admin_created == True)
-        .where(CharacterList.parent_list == None)
-    ).all()
+    top_level_user_lists = lists_service.get_top_level_user_lists()
+    top_level_premade_lists = lists_service.get_top_level_premade_lists()
+
     return render_template('lists.html', user_lists=top_level_user_lists, premade_lists=top_level_premade_lists)
+
+
--- app/character/routes/sublists.py
+++ app/character/routes/sublists.py
@@ -1,46 +1,16 @@
 from flask import render_template
-from flask_login import current_user
-
 from app.character import character
-from app.models import CharacterList
-from app import db
-
-def get_all_parents(list_id):
-    # Create a CTE to recursively get all parents
-    parent_cte = db.session.query(CharacterList).\
-        filter(CharacterList.id == list_id).\
-        cte(recursive=True)
-
-    # Define the recursive query
-    parent_cte = parent_cte.union_all(
-        db.session.query(CharacterList).\
-            filter(CharacterList.id == parent_cte.c.parent_list_id)
-    )
-
-    # Get all parents
-    all_parents = db.session.query(parent_cte).all()
-
-    # Return the parent list objects
-    reversed_parents = all_parents[::-1]
-    return reversed_parents[:-1]
+from app.character.services import lists_service
 
 
 @character.route('/lists/<int:list_id>')
 def sublists(list_id):
     # Parent lists
-    parents = get_all_parents(list_id)
-    #
-
-    current_list = CharacterList.query.get(list_id)
+    parents = lists_service.get_all_parent_lists(list_id)
+    current_list = lists_service.get_list_by_id(list_id)
     if current_list is None:
         return 'List not found', 404
     child_lists = current_list.child_lists
-    print(current_list.parent_list)
-    print('Sublists: ')
-    print(child_lists)
-
-    print('Parents')
-    print(parents)
     characters = current_list.characters
     return render_template('sublists.html', list=current_list, child_lists=child_lists, characters=characters,
                            parents=parents)
