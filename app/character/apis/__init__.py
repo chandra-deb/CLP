21272e8ddefaceba1b39b21984834f9b9cc8dacc
Chandra Deb <chandra.deb.sc@outlook.com>
2024-08-25 03:01:09 +0800

    add delete route

--- app/character/apis/__init__.py
+++ app/character/apis/__init__.py
@@ -2,3 +2,4 @@ from app.character.apis.update_chars_detail import update_chars_detail
 from app.character.apis.create_list import create_list
 from app.character.apis.pin_list import pin_list
 from app.character.apis.unpin_list import unpin_list
+from app.character.apis.delete_list import delete_list
