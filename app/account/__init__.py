96ccc63683c6166ae39c858fe8b74f43b1c6ba7a
Chandra Deb <chandra.deb.sc@outlook.com>
2024-08-31 21:34:22 +0800

    create Select Study Type Modal

--- app/account/__init__.py
+++ app/account/__init__.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 
 #The blueprint name will require when using url_for function.
-account: Blueprint = Blueprint('account', __name__, template_folder='templates', static_folder='static')
+account: Blueprint = Blueprint('account', __name__, template_folder='templates', static_folder='static',
+                               url_prefix='/account')
 
 # Attach registered routes to this blueprint
 from app.account.routes import *
